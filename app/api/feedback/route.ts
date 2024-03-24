45349aa8d2c7c9ec0ad9a8230fe24698e2a63b79
Zaid Sameer <amrikee7@gmail.com>
2024-03-25 00:24:43 +0300

    updating the code

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -1,13 +1,16 @@
 import Feedback from "@/lib/models/feedback";
 import { connectToDB } from "@/lib/db/database";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 export const POST = async (request: any) => {
   const { name, email, category, message } = await request.json();
 
   try {
     await connectToDB();
     const feedback = new Feedback({ name, email, category, message });
-
+    
     await feedback.save();
     return new Response(JSON.stringify(feedback), { status: 201 });
   } catch (error) {
--- components/mainFooter.tsx
+++ components/mainFooter.tsx
@@ -1,4 +1,4 @@
-import { AtSign, Facebook, Flower2, ScanFace } from 'lucide-react';
+import { AtSign, Facebook } from 'lucide-react';
 import React from 'react'
 import Link from 'next/link';
 import { config } from '@/lib/config';
@@ -7,7 +7,7 @@ const MainFooter = () => {
 
   return (
     <div className="mt-10 shadow-md bg-gray-900">
-      <footer className="py-6 text-gray-500 w-full" >
+      <footer className="py-6 text-gray-500 w-full">
         <div className="container px-6 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
           <div className="flex items-center justify-between">
             <div className="col-span-full md:pb-0 md:col-span-6">
@@ -17,19 +17,13 @@ const MainFooter = () => {
               <p className='text-sm'>Copyright © 2005 - {new Date().getFullYear()}</p>
             </div>
             <div className="flex justify-center space-x-4 lg:pt-0 lg:col-end-13">
-              <a
-
-                href={config.mainEmail}
-                title="Email"
+              <a href={config.mainEmail} title="Email"
                 className="flex items-center justify-center w-10 h-10 rounded-full bg-primary text-gray-50"
               >
                 <AtSign />
               </a>
 
-              <Link
-                href={config.facebookURL}
-                target='_blank'
-                title="Facebook"
+              <Link href={config.facebookURL} target='_blank' title="Facebook"
                 className="flex items-center justify-center w-10 h-10 rounded-full bg-primary text-gray-50"
               >
                 <Facebook />
